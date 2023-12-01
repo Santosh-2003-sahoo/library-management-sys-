75b4c073ec0257c8f2a32b4b935e67532128891f
Santosh-2003-sahoo <imsantoshsahoo1430@gmail.com>
2023-12-01 10:38:10 +0530

    uploading files

---
+++ backend/server.js
@@ -0,0 +1,45 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import bookRoutes from "./routes/books.js";
+import transactionRoutes from "./routes/transactions.js";
+import categoryRoutes from "./routes/categories.js";
+
+/* App Config */
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 4000;
+
+/* Middlewares */
+app.use(express.json());
+app.use(cors());
+
+/* API Routes */
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/books", bookRoutes);
+app.use("/api/transactions", transactionRoutes);
+app.use("/api/categories", categoryRoutes);
+
+/* MongoDB connection */
+const uri =
+  "mongodb+srv://1234:santosh@cluster0.f8zvrx8.mongodb.net/?retryWrites=true&w=majority";
+mongoose
+  .connect(uri)
+  .then(() => console.log("Db connect"))
+  .catch((error) => {
+    console.log("ISSUE");
+    console.log(error.message);
+    process.exit(1);
+  });
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to LibraryApp");
+});
+
+/* Port Listening In */
+app.listen(port, () => {
+  console.log(`Server is running in PORT ${port}`);
+});
